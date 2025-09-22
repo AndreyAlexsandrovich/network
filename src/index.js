b9bf360ebf49b24850f2f99f26b5a3b3f1981c79
Adnrey <lomovskoy.andrey@bk.ru>
2025-09-22 23:35:24 +0300

    refactor files home.html searchProducts.html index.html update semantics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import initSideBar from "./scripts/sidebarButton.js";
 import apiProducts from "./scripts/api.js";
 import createProduct from "./scripts/createProduct.js";
-
+import searchProducts from "./scripts/searchProducts.js"
 
 document.addEventListener("DOMContentLoaded", () => {
   initSideBar();
--- webpack.config.js
+++ webpack.config.js
@@ -2,86 +2,90 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
-  entry: {
-    main: "./src/index.js",
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
-    publicPath: "auto",
-  },
-  // mode: isProd ? "production" : "development",
-  devServer: {
-    static: path.resolve(__dirname, "./dist"),
-    compress: true,
-    port: 8080,
-    open: true,
-  },
-
-  stats: {
-    children: true,
-  },
-
-  resolve: {
-    alias: {
-      "@styles": path.resolve(__dirname, "src/styles"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@variables": path.resolve(__dirname, "src/variables"),
-      "@utilities": path.resolve(__dirname, "src/utilities"),
+module.exports = (argv) => {
+  const isProd = argv.mode === "production";
+  const repoName = 'network'
+  return {
+    entry: {
+      main: "./src/index.js",
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-
-            options: { importLoaders: 1, esModule: false },
-          },
-          "postcss-loader",
-        ],
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].js",
+      publicPath: isProd ? `/${repoName}/` : "/",
+    },
+    mode: isProd ? "production" : "development",
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, "dist"),
+        publicPath:  "/",
       },
-      {
-        test: /\.js$/,
-        use: "babel-loader",
-        exclude: "/node_modules/",
+      compress: true,
+      port: 8080,
+      open: true,
+      historyApiFallback: true,
+    },
+    stats: {
+      children: true,
+    },
+    resolve: {
+      alias: {
+        "@styles": path.resolve(__dirname, "src/styles"),
+        "@components": path.resolve(__dirname, "src/components"),
+        "@variables": path.resolve(__dirname, "src/variables"),
+        "@utilities": path.resolve(__dirname, "src/utilities"),
       },
-      {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf|jpeg)$/,
-        type: "asset/resource",
-      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: { importLoaders: 1, esModule: false },
+            },
+            "postcss-loader",
+          ],
+        },
+        {
+          test: /\.js$/,
+          use: "babel-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf|jpeg)$/,
+          type: "asset/resource",
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "index.html",
+        chunks: ["main"],
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/pages/404.html",
+        filename: "404.html",
+        chunks: ["main"],
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/pages/home.html",
+        filename: "home.html",
+        chunks: ["main"],
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/pages/searchProducts.html",
+        filename: "searchProducts.html",
+        chunks: ["main"],
+      }),
+      new MiniCssExtractPlugin({
+        filename: "main.css",
+      }),
+      new CleanWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
-      chunks: ["main"],
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/pages/404.html",
-      filename: "404.html",
-      chunks: ["main"],
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/pages/home.html",
-      filename: "home.html",
-      chunks: ["main"],
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/pages/searchProducts.html",
-      filename: "searchProducts.html",
-      chunks: ["main"],
-    }),
-    new MiniCssExtractPlugin({
-      filename: "main.css",
-    }),
-    new CleanWebpackPlugin(),
-  ],
+  };
 };
