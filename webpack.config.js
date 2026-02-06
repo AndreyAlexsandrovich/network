a5ae6ef5443dbdfb38d570025a7fab4ca04e1db2
AndreyAlexsandrovich <lomovsoy.andrey@bk.ru>
2026-02-06 03:33:57 +0300

    chore: configure webpack for GitHub PAges + local dev

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,30 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-
 module.exports = (argv) => {
+  const isProduction = argv.mode === "production";
+
+  const pages = ["404", "registration", "bookmark", "searchProducts"];
+
+  const htmlPlugins = pages.map(
+    (page) =>
+      new HtmlWebpackPlugin({
+        template: `./src/pages/${page}.html`,
+        filename: `${page}.html`,
+        chunks: ["main"],
+        publicPath: isProduction ? "./" : "/",
+      })
+  );
+
   return {
     entry: {
       main: "./src/index.ts",
-      // styles: './src/index.css',
     },
     output: {
       path: path.resolve(__dirname, "dist"),
-      filename: "[name].js",
-      assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
-      publicPath: './',
-      // clean: true,
+      filename: "[name].[contenthash].js",
+      assetModuleFilename: path.join("images", "[name].[contenthash][ext]"),
+      publicPath: isProduction ? "./" : "/",
     },
     mode: argv.mode || "development",
     devServer: {
@@ -26,7 +37,7 @@ module.exports = (argv) => {
       open: false,
     },
     resolve: {
-      extensions: ['.ts', '.js', '.json'],
+      extensions: [".ts", ".js", ".json"],
       alias: {
         "@styles": path.resolve(__dirname, "src/styles"),
         "@components": path.resolve(__dirname, "src/components"),
@@ -56,63 +67,40 @@ module.exports = (argv) => {
           test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
           type: "asset/resource",
           generator: {
-            filename: path.join('icons', '[name].[contenthash][ext]'),
+            filename: path.join("icons", "[name].[contenthash][ext]"),
           },
         },
         {
           test: /\.ts$/,
-          use: 'ts-loader',
-          exclude: /node_modules/
-        }
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
       ],
-
     },
     plugins: [
       new HtmlWebpackPlugin({
         template: "./src/index.html",
         filename: "index.html",
-        publicPath: './',
-        minify: argv.mode === "production" ? { 
-          removeComments: true,
-          collapseWhitespace: true,
-        } : false,
+        publicPath: isProduction ? "./" : "/",
+        minify:
+          argv.mode === "production"
+            ? {
+                removeComments: true,
+                collapseWhitespace: true,
+              }
+            : false,
         chunks: ["main"],
       }),
-      // new HtmlWebpackPlugin({
-      //   template: "./src/pages/404.html",
-      //   filename: "404.html",
-      //   chunks: ["main"],
-      // }),
-      // new HtmlWebpackPlugin({
-      //   template: "./src/pages/home.html",
-      //   filename: "home.html",
-      //   chunks: ["main"],
-      // }),
-      // new HtmlWebpackPlugin({
-      //   template: "./src/pages/searchProducts.html",
-      //   filename: "searchProducts.html",
-      //   chunks: ["main"],
-      // }),
-      // new HtmlWebpackPlugin({
-      //   template: "./src/pages/registration.html",
-      //   filename: "registration.html",
-      //   chunks: ["main"],
-      // }),
-      // new HtmlWebpackPlugin({
-      //   template: "./src/pages/bookmark.html",
-      //   filename: "bookmark.html",
-      //   chunks: ["main"],
-      // }),
-
+      ...htmlPlugins,
       new MiniCssExtractPlugin({
         filename: "main.[contenthash].css",
       }),
       new CleanWebpackPlugin(),
     ],
-    optimization: { 
-      splitChunks: { 
-        chunks: 'all',
+    optimization: {
+      splitChunks: {
+        chunks: "all",
       },
     },
   };
-}; 
+};
