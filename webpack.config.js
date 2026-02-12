e6099bd22355b33a35f891e214dc6f3e46e2205f
AndreyAlexsandrovich <lomovsoy.andrey@bk.ru>
2026-02-12 09:00:47 +0300

    fix: public Patch webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const publicPath = '/'
+const publicPath = '/network'
 
 
 module.exports = (argv) => {
